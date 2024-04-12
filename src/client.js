c52d0d389315c5d18275a9094d65a2dd592d0846
Ernest Rodriguez <ernestrd@amazon.com>
2024-04-12 18:04:56 -0500

    Adding Cognito resources and logic into the app

--- src/client.js
+++ src/client.js
@@ -1,11 +1,28 @@
 import { SupportClient } from "@aws-sdk/client-support";
+import { fetchAuthSession } from 'aws-amplify/auth';
 
-const client = new SupportClient({
-    region: 'us-east-1',
-    credentials: {
-    accessKeyId: process.env.REACT_APP_partneraccesskey,
-    secretAccessKey: process.env.REACT_APP_partnercasesecretkey
+export const getClient = async () => {
+    try {
+        const session = await fetchAuthSession();
+        const { accessToken, idToken, credentials } = session ?? {};
+        if (credentials) {
+            const client = new SupportClient({
+                region: 'us-east-1',
+                credentials: {
+                    accessKeyId: credentials.accessKeyId,
+                    secretAccessKey: credentials.secretAccessKey,
+                    sessionToken: credentials.sessionToken
+                }
+            });
+            return client;
+        } else {
+            console.error('Credentials are missing in the session.');
+            return null;
+        }
+    } catch (err) {
+        console.log(err);
+        return null;
     }
-  });
+}
 
-export default client ;
+export default getClient;
