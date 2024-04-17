7ae884c863edea2a921bafb4dde175d6318412ae
Ernest Rodriguez <ernestrd@amazon.com>
2024-04-17 10:17:50 -0500

    Readding Amplify config and resources

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "partnercreatecasea5158a64": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
